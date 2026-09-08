2f5e4644e60ebd0e086e8a58840a321eec42318f
meug-gft <meug@gft.com>
2026-09-08 16:44:02 +0200

    comienzo

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,19 @@
 import './App.css'
+import SearchTrip from './SearchTrip'
+import Header from './Header'
 
 function App() {
 
   return (
     <>
-      <div className="w-full">
-        <div className="board-header">
-          <span>Código</span>
-          <span>Destino</span>
-          <span>Paradas</span>
-          <span>Dársena</span>
-          <span>H. Salida</span>
-        </div>
-        <div className="board-row">
-          <span>IC 0123</span>
-          <span>Madrid</span>
-          <span className="min-w-0 overflow-x-auto whitespace-nowrap scrollbar-none">Cella, Sarrión, Rubielos de Mora, Mora de Rubielos, Sagunto, Valencia</span>
-          <span>4</span>
-          <span>14:35</span>
-        </div>
-        <div className="board-row">
-          <span>RE 4821</span>
-          <span>Alicante</span>
-          <div className="min-w-0 overflow-hidden">
-            <div className="flex w-max animate-stops-scroll">
-              <span className="whitespace-nowrap">
-                Valencia · Cuenca · Requena · Utiel · Camporrobles · Madrid
-              </span>
-
-              <span className="ml-8 whitespace-nowrap">
-                Valencia · Cuenca · Requena · Utiel · Camporrobles · Madrid
-              </span>
-            </div>
-          </div>
-          <span>7</span>
-          <span>14:52</span>
-        </div>
-        <div className="board-row">
-          <span>EU 0091</span>
-          <span>Barcelona</span>
-          <span className="min-w-0 overflow-x-auto whitespace-nowrap scrollbar-none">Tarragona</span>
-          <span>2</span>
-          <span>15:10</span>
-        </div>
-      </div>
+      <Header/>
+      <section className='px-4 *:font-[Outfit]'>
+        <SearchTrip/>
+        {/* <DeparturesIssues/> */}
+        <p>adsadsa</p>
+        {/* <News/> */}
+        {/* <Footer/> */}
+      </section>
     </>
   )
 }
---
+++ src/Header.tsx
@@ -0,0 +1,8 @@
+
+export default function Header() {
+    return(
+        <div className="w-full bg-main-600">
+            <p className='font-[Outfit] text-white text-4xl text-center uppercase p-2'>La Estación</p>
+        </div>
+    )
+}
